099e32778c38227ae033e07ab9311139d17b4751
joeljames <joeljames1985@gmail.com>
2017-06-20 08:37:55 -0700

    Configure ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify'); // Bundle JS
 var reactify = require('reactify'); // Transforms React JSX to JS
 var source = require('vinyl-source-stream'); // Use conventional text stream with Gulp
 var concat = require('gulp-concat'); // Concatinates files
+var lint = require('gulp-eslint'); // Lint JS files, include JSX
 
 var config = {
     port: 9005,
@@ -60,9 +61,15 @@ gulp.task('css', function(){
         .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
+gulp.task('lint', function(){
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format());
+})
+
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);

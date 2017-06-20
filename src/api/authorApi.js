5a1cf1e3fd990ba246f7191fee9b57a442b4befc
joeljames <joeljames1985@gmail.com>
2017-06-20 15:18:02 -0700

    Add mock API and create React component to call the mock API

---
+++ src/api/authorApi.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var authors = require('./authorData');
+var _ = require('lodash');
+
+var _generateId = function(author) {
+    return author.firstName.toLowerCase() + '-' + author.lastName.toLowerCase();
+};
+
+var _clone = function(item) {
+    return JSON.parse(JSON.stringify(item));
+};
+
+var AuthorApi = {
+    getAllAuthors: function() {
+        return _clone(authors.authors);
+    },
+
+    getAuthorById: function(id) {
+        var author = _.find(authors, {id: id});
+        return _clone(author);
+    },
+
+    saveAuthor: function(author) {
+        //pretend a ajax call to web api is made here
+        console.log('Pretend this just saved the author to DB via AJAX call...');
+
+        if (author.id) {
+            var existingAuthorIndex = _.indexOf(authors, _.find(authors, {id: author.id}));
+            authors.splice(existingAuthorIndex, 1, author);
+        } else {
+            // Just simulate creation here
+            author.id = _generateId(author);
+            authors.push(author);
+        }
+        return _clone(author);
+    },
+
+    deleteAuthor: function(id) {
+        console.log('Pretend this just deletes the author to DB via AJAX call...');
+        _.remove(authors, {id: id});
+    }
+};
+
+module.exports = AuthorApi;
---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 'joel-james',
+            firstName: 'Joel',
+            lastName: 'James'
+        },
+        {
+            id: 'mark-james',
+            firstName: 'Mark',
+            lastName: 'James'
+        },
+        {
+            id: 'john-smith',
+            firstName: 'John',
+            lastName: 'Smith'
+        }
+    ]
+};
